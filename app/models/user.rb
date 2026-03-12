26e35ff1edf3a68709cff1c174b946f6c58fc838
endo-202588 <stb5181@gmail.com>
2026-03-12 17:18:01 +0900

    add:flash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :posts
+
   validates :password,
             length: { minimum: 4 },
             if: -> { new_record? || will_save_change_to_crypted_password? }
